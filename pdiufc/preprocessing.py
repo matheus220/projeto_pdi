d66954b5f49884864f3e92372dbdc58cd5526767
danielfilhocoelho <danielfilhocoelho@users.noreply.github.com>
2019-11-30 17:19:50 -0300

    Update preprocessing.py

--- pdiufc/preprocessing.py
+++ pdiufc/preprocessing.py
@@ -2,7 +2,7 @@ from skimage.filters import median
 from skimage.morphology import disk
 import cv2
 
-def process(image):
+def preprocess(image):
     ret,thresh1 = cv.threshold(image[:,:,1],127,255,cv.THRESH_BINARY)
     med = median(thresh1, disk(5))
     return med
