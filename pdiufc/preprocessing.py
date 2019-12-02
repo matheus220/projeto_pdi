90b3e0b3330b4a133999237af768a7472f3fd1ac
danielfilhocoelho <danielfilhocoelho@users.noreply.github.com>
2019-12-02 19:20:50 -0300

    Update preprocessing.py

--- pdiufc/preprocessing.py
+++ pdiufc/preprocessing.py
@@ -3,7 +3,11 @@ from skimage.morphology import disk
 import cv2
 
 def preprocess(image):
-    ret,thresh1 = cv.threshold(image[:,:,1],127,255,cv.THRESH_BINARY)
+    # FILTRO DE MÉDIA PARA BORRAR BACKGROUND
+    blur15 = cv2.blur(image[:,:,1],(10,10))
+    # LIMIARIZAÇÃO
+    ret,thresh1 = cv.threshold(image[:,:,1],160,255,cv.THRESH_BINARY)
+    # FILTRO DE MEDIANA PARA ELIMINAR RUÍDO SAL-E-PIMENTA RESIDUAL
     med = median(thresh1, disk(5))
     return med
 
