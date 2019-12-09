bda38cc61ca4f64d3b66dfa731cb839fb3f395ba
danielfilhocoelho <danielfilhocoelho@users.noreply.github.com>
2019-12-09 00:12:47 -0300

    Update preprocessing.py

--- pdiufc/preprocessing.py
+++ pdiufc/preprocessing.py
@@ -5,9 +5,10 @@ import cv2
 def preprocess(image):
     # FILTRO DE MÉDIA PARA BORRAR BACKGROUND
     blur15 = cv2.blur(image[:,:,1],(10,10))
+    #blur15 = cv2.GaussianBlur(image[:,:,1],(5,5),8)
     # LIMIARIZAÇÃO
-    ret,thresh1 = cv.threshold(image[:,:,1],160,255,cv.THRESH_BINARY)
+    ret,thresh1 = cv.threshold(blur15,160,255,cv.THRESH_BINARY)
+    #ret,thresh1 = cv2.threshold(blur15,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     # FILTRO DE MEDIANA PARA ELIMINAR RUÍDO SAL-E-PIMENTA RESIDUAL
     med = median(thresh1, disk(5))
     return med
-
