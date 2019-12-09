7aa43540eb729622b56702ad0102790c4967be3c
Allan C. Marques <acmlira@gmail.com>
2019-12-09 20:47:48 -0300

    Interface team work

---
+++ interface/camera_opencv.py
@@ -0,0 +1,36 @@
+import os
+import cv2
+from base_camera import BaseCamera
+from func import contorno2
+
+class Camera(BaseCamera):
+    video_source = 0
+
+    def __init__(self, video_source='video_original.avi'):
+        # BaseCamera.thread=None
+        Camera.set_video_source(video_source)
+        if os.environ.get('OPENCV_CAMERA_SOURCE'):
+            Camera.set_video_source(int(os.environ['OPENCV_CAMERA_SOURCE']))
+        super(Camera, self).__init__()
+
+    @staticmethod
+    def set_video_source(source):
+        Camera.video_source = source
+
+    @staticmethod
+    def frames():
+        alfa = 5 # alfa é o limiar de angulo para que apareça um contorno
+        count = 0
+        camera = cv2.VideoCapture(Camera.video_source)
+        if not camera.isOpened():
+            raise RuntimeError('Could not start camera.')
+
+        while True:
+            ret, frame = camera.read()
+            _, frame_original = camera.read()
+            img = contorno2(frame, frame_original, alfa)
+            cv2.rectangle(img, (50, 0), (300, 720), (0, 255, 0), 2)
+            # cv2.imshow('frame', img)
+            if (cv2.waitKey(30) != -1):  # aperta ESC e a janela fecha
+                break
+            yield cv2.imencode('.jpg', img)[1].tobytes()
---
+++ interface/process_video.py
@@ -0,0 +1,20 @@
+import cv2
+from func import contorno2
+cap = cv2.VideoCapture("fundo_preto_torrada_branca.avi")
+cap_original = cv2.VideoCapture('video_original.avi')
+
+
+def show_original_video(alfa):
+    while True:
+        ret, frame = cap.read()
+        _, frame_original = cap_original.read()
+        cv2.rectangle(frame_original, (50, 0), (300, 670), (0, 255, 0), 2)
+        if (ret):
+            frame = contorno2(frame_original, alfa)
+            cv2.imshow('frame', frame)
+        if (cv2.waitKey(30) != -1):  # aperta ESC e a janela fecha
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+show_original_video(5)
