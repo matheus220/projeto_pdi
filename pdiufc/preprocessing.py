757a7620bcdc9630fe00d57a56c1977f8626bee7
danielfilhocoelho <danielfilhocoelho@users.noreply.github.com>
2019-12-09 00:13:18 -0300

    Update preprocessing.py

--- pdiufc/preprocessing.py
+++ pdiufc/preprocessing.py
@@ -7,7 +7,7 @@ def preprocess(image):
     blur15 = cv2.blur(image[:,:,1],(10,10))
     #blur15 = cv2.GaussianBlur(image[:,:,1],(5,5),8)
     # LIMIARIZAÇÃO
-    ret,thresh1 = cv.threshold(blur15,160,255,cv.THRESH_BINARY)
+    ret,thresh1 = cv2.threshold(blur15,160,255,cv2.THRESH_BINARY)
     #ret,thresh1 = cv2.threshold(blur15,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     # FILTRO DE MEDIANA PARA ELIMINAR RUÍDO SAL-E-PIMENTA RESIDUAL
     med = median(thresh1, disk(5))
